fcad328eefdf82709c4a3d131096ef688f687e22
Wiggyboy <Wiggy0boy@gmail.com>
2020-03-12 20:07:53 +0100

    Create test code for a motor

--- Software/src/main.cpp
+++ Software/src/main.cpp
@@ -5,11 +5,40 @@ int ledPin = LED_BUILTIN;
 
 void setup() {
     pinMode(ledPin, OUTPUT);
+
+    // Motor
+    pinMode(2, OUTPUT);
+    pinMode(3, OUTPUT);
+
+    // pinMode(11, ANALOG);
+
+    digitalWrite(2, HIGH);
+    digitalWrite(3, LOW);
 }
 
 void loop() {
-    digitalWrite(ledPin, HIGH);
-    delay(500 / frequency);
-    digitalWrite(ledPin, LOW);
-    delay(500 / frequency);
+    if (0) {
+        digitalWrite(ledPin, HIGH);
+        delay(500 / frequency);
+        digitalWrite(ledPin, LOW);
+        delay(500 / frequency);
+    }
+
+    // Motor
+    int numberOfSteps = 20;
+    int stepTime = 100;
+
+    for (int i = 0; i <= numberOfSteps; i++) {
+        analogWrite(11, (double) i/numberOfSteps * 255);
+        delay(stepTime);
+    }
+
+
+    for (int i = 0; i <= numberOfSteps; i++) {
+        analogWrite(11, (double) (numberOfSteps - i)/numberOfSteps * 255);
+        delay(stepTime);
+    }
+
+    delay(1000);
 }
+
