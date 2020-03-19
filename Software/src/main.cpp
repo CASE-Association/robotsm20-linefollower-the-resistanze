53608922a3e6a1cc999a22b138d6468e88ff2c1b
Wiggyboy <Wiggy0boy@gmail.com>
2020-03-19 14:12:27 +0100

    Let main use Motor instead of old implementation

--- Software/src/main.cpp
+++ Software/src/main.cpp
@@ -4,42 +4,39 @@
 int frequency = 4;
 int ledPin = LED_BUILTIN;
 
+int PWMA = 11;
+int AIN1 = 2;
+int AIN2 = 3;
+Motor motorA;
+
 void setup() {
+    //Serial.begin(9600);
     pinMode(ledPin, OUTPUT);
 
     // Motor
-    pinMode(2, OUTPUT);
-    pinMode(3, OUTPUT);
-
-    // pinMode(11, ANALOG);
-
-    digitalWrite(2, HIGH);
-    digitalWrite(3, LOW);
+    motorA = Motor(PWMA, AIN1, AIN2);
 }
 
 void loop() {
-    if (0) {
-        digitalWrite(ledPin, HIGH);
-        delay(500 / frequency);
-        digitalWrite(ledPin, LOW);
-        delay(500 / frequency);
-    }
 
     // Motor
     int numberOfSteps = 20;
     int stepTime = 100;
 
     for (int i = 0; i <= numberOfSteps; i++) {
-        analogWrite(11, (double) i/numberOfSteps * 255);
+        int speed = (double) i/numberOfSteps * 255;
+
+        motorA.forward(speed);
         delay(stepTime);
     }
 
 
     for (int i = 0; i <= numberOfSteps; i++) {
-        analogWrite(11, (double) (numberOfSteps - i)/numberOfSteps * 255);
+        int speed = (double) (numberOfSteps - i)/numberOfSteps * 255;
+
+        motorA.forward(speed);
         delay(stepTime);
     }
 
     delay(1000);
 }
-
